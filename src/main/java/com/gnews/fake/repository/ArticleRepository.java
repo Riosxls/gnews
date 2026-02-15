b8c9cedde4be6e7998838d9437d72d57cf465a21
Italo Rios <italo.rios@outlook.de>
2026-02-14 21:25:29 -0300

    Update ArticleRepository.java

--- src/main/java/com/gnews/fake/repository/ArticleRepository.java
+++ src/main/java/com/gnews/fake/repository/ArticleRepository.java
@@ -18,4 +18,11 @@ public class ArticleRepository {
     public List<Article> findAll() {
         return Collections.unmodifiableList(articles);
     }
+
+    public List<News> findByTitle(String userInput) {
+    String query = "SELECT * FROM news WHERE title = '" + userInput + "'";
+    return jdbcTemplate.query(query, new NewsRowMapper());
 }
+}
+
+
